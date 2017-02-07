a139b86f9d709f5e74b4825b9ec19d13ea00d2de
mechadojo <mechadojo@gmail.com>
2017-02-06 23:40:43 -0600

    Added Odometry Test OpMode

---
+++ OdometryTestAuto.java
@@ -0,0 +1,223 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.robocol.TelemetryMessage;
+
+import org.mechadojo.navigation.EncoderMessage;
+import org.mechadojo.navigation.TwoWheelOdometry;
+import org.mechadojo.stateflow.Controller;
+import org.mechadojo.stateflow.Message;
+import org.mechadojo.stateflow.examples.DelayedHelloWorldController;
+import org.mechadojo.stateflow.opmode.StateFlowOpMode;
+
+import java.io.FileOutputStream;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@TeleOp(name = "Odometry Test ", group = "Test")
+public class OdometryTestAuto extends StateFlowOpMode {
+    Hardware howler = new Hardware();
+    Sensors sensors = new Sensors();
+    TwoWheelOdometry odometry = new TwoWheelOdometry();
+    Thread odometryThread;
+    double maxSpeed = 0.0;
+
+    @Override
+    public Controller loadController() {
+        return new Controller();
+    }
+
+    @Override
+    public void init() {
+        super.init();
+
+        howler.init(hardwareMap);
+        howler.setCoastMode(true);
+        sensors.init(hardwareMap);
+
+        odometry.controller = controller;
+        odometry.leftMotor = howler.leftBack;
+        odometry.rightMotor = howler.rightBack;
+
+        odometry.leftWheelFactor = -8.0 / 3752.0;
+        odometry.rightWheelFactor = -8.0 / 3754.0;
+
+        if (!odometry.running) {
+            odometryThread = new Thread(odometry);
+            odometryThread.start();
+        }
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        odometry.running = false;
+        super.stop();
+    }
+
+    @Override
+    public void loop() {
+        handleTeleop();
+        super.loop();
+    }
+
+    public void updateOdometryTelemetry() {
+
+        double avg = odometry.stats.mean();
+        double stddev = odometry.stats.standardDeviation();
+
+        telemetry.addData("Odometry", String.format("%.2f msec σ: %.4f)", avg, stddev));
+        Message m = controller.getParameter("odometry/left_wheel");
+        if (m != null && m instanceof  EncoderMessage) {
+            updateEncoderTelemetry("Left", (EncoderMessage)m);
+        }
+
+        m = controller.getParameter("odometry/right_wheel");
+        if (m != null && m instanceof  EncoderMessage) {
+            updateEncoderTelemetry("Right", (EncoderMessage)m);
+        }
+
+        telemetry.addData("Max Speed:", maxSpeed);
+    }
+
+    public void updateEncoderTelemetry(String caption, EncoderMessage msg) {
+        telemetry.addData(caption, String.format("T:%d  P:%.4f  S:%.4f  A:%.4f", msg.ticks, msg.position, msg.speed, msg.acceleration));
+        if (Math.abs(msg.speed) > maxSpeed) maxSpeed = Math.abs(msg.speed);
+    }
+
+    public void updateInitTelemetry() {
+        super.updateInitTelemetry();
+        updateOdometryTelemetry();
+    }
+
+    public void updateRunTelemetry() {
+        super.updateRunTelemetry();
+
+        double avg = odometry.stats.mean();
+        double stddev = odometry.stats.standardDeviation();
+
+        updateOdometryTelemetry();
+    }
+
+
+    double deadZone = 0.15;
+    boolean yButtonPressed = false;
+    boolean buttonMode = false;
+
+    boolean startPressed = false;
+    boolean teleMode = true;
+
+    public void handleTeleop() {
+
+        sensors.updateSensors();
+
+        double drivePower = gamepad1.left_stick_y;
+        double turnPower = -gamepad1.right_stick_x;
+
+        if(Math.abs(drivePower) < deadZone) {
+            drivePower = 0;
+        } else {
+            if (drivePower > 0) drivePower -= deadZone;
+            else drivePower += deadZone;
+        }
+
+        if(Math.abs(turnPower) < deadZone) {
+            turnPower = 0;
+        } else {
+            if (turnPower > 0) drivePower -= deadZone;
+            else turnPower += deadZone;
+        }
+
+        if(Math.abs(turnPower) < 0.95) {
+            if(Math.abs(drivePower) < 0.25) {
+                turnPower *= 0.60;
+            }
+            else {
+                turnPower *= 0.80;
+            }
+        }
+
+        if(Math.abs(drivePower) < 0.95) {
+            drivePower *= 0.75;
+        }
+
+        double collectPower;
+        if(gamepad1.right_trigger > deadZone || gamepad2.right_trigger > deadZone) {
+            collectPower = 1;
+        }
+        else if(gamepad1.left_trigger > deadZone || gamepad2.left_trigger > deadZone){
+            collectPower = -1;
+        }
+        else {
+            collectPower = 0;
+        }
+
+        double shootPower;
+        if(gamepad2.right_bumper || gamepad1.right_bumper) {
+            shootPower = 1;
+        }
+        else if(gamepad2.left_bumper || gamepad1.left_bumper) {
+            shootPower = 0.5;
+        }
+        else {
+            shootPower = 0;
+        }
+
+        double leftFlick;
+        if(gamepad2.x || gamepad1.x) {
+            leftFlick = Hardware.LEFT_FLICKER_UP;
+        }
+        else {
+            leftFlick = Hardware.LEFT_FLICKER_DOWN;
+        }
+
+        double rightFlick;
+        if(gamepad2.b || gamepad1.b) {
+            rightFlick = Hardware.RIGHT_FLICKER_UP;
+        }
+        else {
+            rightFlick = Hardware.RIGHT_FLICKER_DOWN;
+        }
+
+        if(gamepad1.dpad_down) {
+            howler.resetShooter(true);
+        }
+
+        if(gamepad1.dpad_up) {
+            howler.resetEncoders(true);
+        }
+
+        if(gamepad1.start && !startPressed) {
+            teleMode = !teleMode;
+        }
+        startPressed = gamepad1.start;
+
+        if(gamepad1.y && !yButtonPressed) {
+            buttonMode = !buttonMode;
+        }
+        yButtonPressed = gamepad1.y;
+
+        if(buttonMode) {
+            drivePower *= -0.5;
+        }
+
+        if(buttonMode) {
+            drivePower *= -0.5;
+        }
+
+        double leftPower = drivePower + turnPower;
+        double rightPower = drivePower - turnPower;
+
+        howler.moveServo(howler.leftFlicker, leftFlick);
+        howler.moveServo(howler.rightFlicker, rightFlick);
+        howler.drive(leftPower, rightPower);
+        howler.collect(collectPower);
+        howler.shoot(shootPower);
+
+    }
+}
--- StateFlowAuto.java
+++ StateFlowAuto.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.mechadojo.stateflow.Controller;
@@ -9,15 +11,26 @@ import org.mechadojo.stateflow.examples.GamepadHelloWorldController;
 import org.mechadojo.stateflow.examples.HelloWorldController;
 import org.mechadojo.stateflow.opmode.StateFlowOpMode;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 @TeleOp(name = "State Flow ", group = "Test")
 public class StateFlowAuto extends StateFlowOpMode {
     @Override
     public Controller loadController() {
-        return new GamepadHelloWorldController();
+        return new DelayedHelloWorldController();
     }
 
     @Override
     public void start() {
+        try {
+            FileOutputStream os = new FileOutputStream("/sdcard/FIRST/test.txt");
+            os.write("This is another test!".getBytes());
+            os.close();
+        }catch (Exception ex) {
+            Log.e("StateFlow", "open file: ", ex );
+        }
+
         super.start();
         //controller.addMessage( "IN helloworld.step_1()" );
     }
